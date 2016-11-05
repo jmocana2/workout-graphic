0a8e1568d5f02401824b73dcaae7cf91f2dc4fc6
Jose Maria <telecomu4@gmail.com>
2016-11-05 13:43:43 +0100

    añadido servidor web express

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000, function(err) {
+	if(err) {
+		return console.log('Hubo un error: ' + err), process.exit(1);
+	}
+	console.log("servidor levantado y escuchando en el puerto 3000...")
+})
